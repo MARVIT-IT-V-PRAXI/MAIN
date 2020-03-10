ad379bff4161196d9647772d203ef2290e1d53ad
w1lo <60324080+w1lo@users.noreply.github.com>
2020-03-10 11:56:02 +0100

    Started working on local database

---
+++ ProductDB/DBLoader.java
@@ -0,0 +1,39 @@
+
+
+
+class DBLoader{
+
+    public DBLoader(String Source){
+
+
+
+
+    }
+
+
+    //String GetItem(String key)
+    //
+    //Return databaseEntry object asociated with given key
+    databaseEntry GetItem(String key){
+    }
+
+
+    boolean AddItem(String key, String value){
+    }
+
+
+
+}
+
+
+class databaseEntry{
+
+
+}
+
+
+enum DBDataType {
+
+    Str, Int, Img //TOTO: Expand The list
+
+}
